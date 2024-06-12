901ab10c07ead663669bf785cdfc9ab76786dc10
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-12 12:57:01 -0600

    Update CSC450_CT1_mod1-2.cpp

--- Module 1/src/CSC450_CT1_mod1-2.cpp
+++ Module 1/src/CSC450_CT1_mod1-2.cpp
@@ -8,7 +8,7 @@ int main()
 {
      double myMoney = 1000.50; // this should be printed out
 
-     // Standard Ouput Statement
+     // Standard Output Statement
      cout << "Please be sure to correct all syntax errors in this program" << endl;
      cout << "I have corrected all errors for this program." << endl;
      cout << "The total amount of money available is = " << myMoney << endl;
