fef4beff8fb35e76bbd1a70e49e2670aa3fe5e5f
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-12 11:43:23 -0600

    add project

---
+++ Module 1/README.md
@@ -0,0 +1,20 @@
+# Module 1
+
+## Usage
+1. Ensure you are in the correct directory of the sub-project.
+2. Run ```cmake -S . -B build``` to generate build files.
+3. Run ```cd build``` to enter build directory.
+4. Depending on the generator use corresponding command to build the binaries of the project.
+    - For Visual Studio Generators run ```MSBuild Module_1.sln``` 
+    - For Makefile Generator run ```make```
+5. Enjoy the program(s)!
+
+## Examples
+Image of fictional character program running.
+![](./example-1.png) <br>
+Image of "CSC450_CT1_mod1-1" program running.
+![](./example-2.png) <br>
+Image of "CSC450_CT1_mod1-2" program running.
+![](./example-3.png) <br>
+
+## Note
---
+++ Module 1/src/FictionalCharacter.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+std::string getHandledInput(std::string prompt)
+{
+    std::string input = "";
+    std::cout << prompt << std::endl;
+    std::getline(std::cin, input);
+
+    if (input.empty())
+    {
+        std::cout << "Empty input! Try again." << std::endl;
+        return getHandledInput(prompt);
+    }
+    return input;
+}
+
+int main()
+{
+    // Input
+    std::string firstName = getHandledInput("Enter the first name of this fictional charater.");
+    std::string lastName = getHandledInput("Enter the last name of this fictional charater.");
+    std::string streetAddress = getHandledInput("Enter the street address of this fictional charater.");
+    std::string city = getHandledInput("Enter the city of this fictional charater.");
+    std::string zipCode = getHandledInput("Enter the zip code of this fictional charater."); // zip codes are ordered numbers
+
+    // Output
+    std::cout << "The character's first name is " << firstName << std::endl;
+    std::cout << "The character's last name is " << lastName << std::endl;
+    std::cout << "The character's street address is " << streetAddress << std::endl;
+    std::cout << "The character's city is " << city << std::endl;
+    std::cout << "The character's zip code is " << zipCode << std::endl;
+
+    return 0;
+}
