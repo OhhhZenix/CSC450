b7f5d9c37de1801dd6ca8fd297441e2223815f95
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-12 11:52:18 -0600

    Update CSC450_CT1_mod1-2.cpp

--- Module 1/src/CSC450_CT1_mod1-2.cpp
+++ Module 1/src/CSC450_CT1_mod1-2.cpp
@@ -1,27 +1,18 @@
+#include <iostream>
 
-
-#include<iostream>
-#include<conio.h>
-
-Standard namespace declaration
+// Standard namespace declaration
 using namespace std;
 
-Main Function
+// Main Function
 int main()
 {
+     double myMoney = 1000.50; // this should be printed out
 
-     double myMoney = 1000.50;  //this should be printed out
+     // Standard Ouput Statement
+     cout << "Please be sure to correct all syntax errors in this program" << endl;
+     cout << "I have corrected all errors for this program." << endl;
+     cout << " The total amount of money available is = " << myMoney << endl;
 
-
-     Standard Ouput Statement
-     cout"Please be sure to correct all syntax errors in this program"<<endl;
-
-     cout<<"I have corrected all errors for this program. <<endl;
-
-     cout<<" The total amount of money available is = "<<   <<endl;
-
-     // Wait For Output Screen
-
-     Main Function return Statement
+     //  Main Function return Statement
      return 0;
 }
