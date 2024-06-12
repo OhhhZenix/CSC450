dc8c71faad9bf59c102e2f2a40e1426d4bbe855f
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-12 11:54:50 -0600

    Update CSC450_CT1_mod1-2.cpp

--- Module 1/src/CSC450_CT1_mod1-2.cpp
+++ Module 1/src/CSC450_CT1_mod1-2.cpp
@@ -11,7 +11,7 @@ int main()
      // Standard Ouput Statement
      cout << "Please be sure to correct all syntax errors in this program" << endl;
      cout << "I have corrected all errors for this program." << endl;
-     cout << " The total amount of money available is = " << myMoney << endl;
+     cout << "The total amount of money available is = " << myMoney << endl;
 
      //  Main Function return Statement
      return 0;
