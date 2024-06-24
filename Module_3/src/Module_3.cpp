2c31c6b1d3b6c090496b122cd4038166b7a941ae
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-24 13:44:57 -0600

    Update Module_3.cpp

--- Module_3/src/Module_3.cpp
+++ Module_3/src/Module_3.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
+#include <string>
+
+int getIntegerInput(std::string prompt) {
+  try {
+    std::string input;
+    std::cout << prompt << std::endl;
+    std::getline(std::cin, input);
+    return std::stoi(input);
+  } catch (const std::exception &e) {
+    std::cout << "Invalid input. Try again!" << std::endl;
+    return getIntegerInput(prompt);
+  }
+}
 
 int main() {
-  int numOne;
-  int numTwo;
-  int numThree;
+  int numOne = getIntegerInput("Enter a number.");
+  int numTwo = getIntegerInput("Enter a second number.");
+  int numThree = getIntegerInput("Enter a third number.");
 
-  int *ptrOne = new int;
-  int *ptrTwo = new int;
-  int *ptrThree = new int;
+  // allocate mem and initialize
+  int *ptrOne = new int(numOne);
+  int *ptrTwo = new int(numTwo);
+  int *ptrThree = new int(numThree);
+
+  std::cout << "Number One - Mem: " << ptrOne << " Value: " << *ptrOne
+            << std::endl;
+  std::cout << "Number Two - Mem: " << ptrTwo << " Value: " << *ptrTwo
+            << std::endl;
+  std::cout << "Number Three - Mem: " << ptrThree << " Value: " << *ptrThree
+            << std::endl;
+
+  // deallocate mem
+  delete ptrOne;
+  delete ptrTwo;
+  delete ptrThree;
+
   return 0;
 }
