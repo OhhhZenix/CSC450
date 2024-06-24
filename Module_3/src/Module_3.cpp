9ebf39c291b03944075793654c563e4840b44182
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2024-06-24 12:28:59 -0600

    Update Module_3.cpp

--- Module_3/src/Module_3.cpp
+++ Module_3/src/Module_3.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 
-int main() { return 0; }
+int main() {
+  int numOne;
+  int numTwo;
+  int numThree;
+
+  int *ptrOne = new int;
+  int *ptrTwo = new int;
+  int *ptrThree = new int;
+  return 0;
+}
